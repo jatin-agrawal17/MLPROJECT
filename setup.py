5aab38c6f2e7369aeaf165aa6e8d6fe53aee4ae6
jatin-agrawal17 <151388565+jatin-agrawal17@users.noreply.github.com>
2025-06-01 01:26:11 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
-from setuptools import find_packages,setup
-from typing import List
 
+# Importing necessary functions and types
+from setuptools import find_packages, setup  # Used to package and install Python projects
+from typing import List  # Used to specify the return type of a function
+
+# Defining a constant for checking '-e .' in requirements.txt
 HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        [req.replace("\n" , "")for req in requirements]
 
+# Function to read and return the list of requirements from requirements.txt
+def get_requirements(file_path: str) -> List[str]:  
+    '''
+    This function will return a list of packages mentioned in the requirements file
+    '''
+    requirements = []  # Empty list to store each requirement
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()  # Read all lines from the file
+        requirements = [req.replace("\n", "") for req in requirements]  # Remove newline characters
+
+        # Remove '-e .' if it's in the list (it's only needed during development, not installation)
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements  # Return the cleaned list of requirements
 
 
+# Calling the setup function to define the package details
 setup(
-    name = 'ML Project',
-    version = '0.0.1',
-    author = 'jatin',
-    author_email = 'agrawaljatin405@gmail.com',
-    packages=find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    name='ML Project',  # Name of your project
+    version='0.0.1',  # Version of your project (can be changed as you update)
+    author='jatin',  # Your name as the author
+    author_email='agrawaljatin405@gmail.com',  # Your contact email
+    packages=find_packages(),  # Automatically find all packages (folders with __init__.py)
+    install_requires=get_requirements('requirements.txt')  # Install dependencies listed in requirements.txt
 )
---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys  # Import sys to get exception details like line number and file name
+import logging
+
+# Function to format a detailed error message
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()  # Get traceback object
+    file_name = exc_tb.tb_frame.f_code.co_filename  # Get the name of the file where the error occurred
+    error_message = "ERROR OCCURED IN PYTHON SCRIPT name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)  # Use line number and error message
+    )
+    return error_message  # Return the formatted string
+
+# Create a custom exception class
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)  # Call the base class constructor
+        self.error_message = error_message_details(error_message, error_detail=error_detail)  # Create detailed message
+
+    def __str__(self):
+        return self.error_message  # What to print when this exception is shown
+    
+
+
+
+    
