beca02ddfbede438822b09022a9e0535630f3f10
jatin-agrawal17 <151388565+jatin-agrawal17@users.noreply.github.com>
2025-06-03 02:50:31 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -21,12 +21,4 @@ class CustomException(Exception):
     
 
 
-if __name__ == '__main__':
-
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info('divide by zero')
-        raise CustomException(e,sys)
-
     
